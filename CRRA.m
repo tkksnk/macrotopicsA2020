a01856b8790b2efa187aecd9a2a846a5b1803d71
Takeki Sunakawa <takeki.sunakawa@gmail.com>
2020-11-10 17:30:00 +0900

    ch 4

--- CRRA.m
+++ CRRA.m
@@ -1,14 +1,8 @@
 function util = CRRA(cons, gamma)
-% Function CRRA
-%  utility = CRRA(consumption, gamma)
-%
-% Purpose:
-%  Compute CRRA utility function
 
 if gamma ~= 1
-    util = cons.^(1-gamma)./(1-gamma);
+%     util = cons.^(1-gamma)./(1-gamma);
+    util = (cons.^(1-gamma)-1)./(1-gamma);
 else
     util = log(cons);
 end
-
-return;
